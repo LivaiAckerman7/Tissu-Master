3473be7ac5d36b3c51bbc23ea1282d8a65a9910e
Samuel Atohoun <samuelatohoun2@gmail.com>
2025-06-04 15:29:20 +0000

    Proxy

--- tissu-master/src/setupProxy.js
+++ tissu-master/src/setupProxy.js
@@ -2,7 +2,7 @@ const { createProxyMiddleware } = require('http-proxy-middleware');
 
 module.exports = function(app) {
     app.use('/api', createProxyMiddleware({
-        target: 'http://localhost:5000',
+        target: 'http://192.168.1.85:5101',
         changeOrigin: true,
         secure: false,
     }));
